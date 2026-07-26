797db4fae97dad450b768cb8242bf2c6db313270
Gaurav kushwaha <gaurav121212kush@gmail.com>
2026-07-26 08:39:16 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.62%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long firstMax = LLONG_MIN; //2
+        long long secondMax  = LLONG_MIN; //1
+        long long thirdMax = LLONG_MIN;
+
+        for(int i=0; i<nums.size(); i++){
+            if( nums[i] == firstMax || nums[i] == secondMax || nums[i] == thirdMax){
+                continue;
+            }
+            if( nums[i] > firstMax ){
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = nums[i];
+
+            }
+            else if( nums[i] < firstMax && nums[i] > secondMax ){
+                thirdMax = secondMax;
+                secondMax = nums[i];
+            }
+            else if (nums[i] < secondMax && nums[i] > thirdMax ){
+                thirdMax = nums[i];
+            }
+        }
+        if( thirdMax == LLONG_MIN ){
+            return firstMax;
+        }
+        else{
+            return thirdMax;
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
