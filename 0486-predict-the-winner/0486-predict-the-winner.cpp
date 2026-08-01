3a915fd5f94929b97a36680bd119a93ec2f103b1
Gaurav kushwaha <gaurav121212kush@gmail.com>
2026-08-01 09:51:15 +0530

    Time: 0 ms (100%), Space: 10.1 MB (52.91%) - LeetHub - Time: O(n^2), Space: O(n^2)

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int i, int j, vector<vector<int>>& dp) {
+
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != INT_MIN)
+            return dp[i][j];
+
+        int takeLeft = nums[i] - solve(nums, i + 1, j, dp);
+        int takeRight = nums[j] - solve(nums, i, j - 1, dp);
+
+        return dp[i][j] = max(takeLeft, takeRight);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+
+        return solve(nums, 0, n - 1, dp) >= 0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
