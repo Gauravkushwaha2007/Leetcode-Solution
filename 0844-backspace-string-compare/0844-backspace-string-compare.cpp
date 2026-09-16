34e9e9df157b75ff7fcea0c754c5c40bd9520a7a
Gaurav kushwaha <gaurav121212kush@gmail.com>
2026-09-16 13:08:47 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.61%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        
+        int i = 0;
+        while ( i < s.size() ) {
+            if ( s[i] == '#') {
+                if ( i > 0 ) {
+                    s.erase(i-1, 2);
+                    i--;
+                } else{
+                    s.erase(i,1);
+                }
+            }
+            else {
+                i++;
+            }
+        }
+        i = 0; 
+
+        while ( i < t.size() ) {
+            if ( t[i] == '#') {
+                if ( i > 0 ) {
+                    t.erase(i-1, 2);
+                    i--;
+                } else {
+                    t.erase(i, 1);
+                }
+            }else{
+                i++;
+            }
+        }
+
+        return ( s == t );
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
