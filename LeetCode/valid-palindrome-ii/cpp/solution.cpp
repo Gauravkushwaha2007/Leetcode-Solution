0325fbdd1d6d276bd2d0b4a601636394b32665ea
Gaurav kushwaha <gaurav121212kush@gmail.com>
2026-09-21 19:46:49 +0530

    Add valid-palindrome-ii (cpp)

---
+++ LeetCode/valid-palindrome-ii/cpp/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    bool check( string &s, int st, int end) {
+        while ( st < end ) {
+            if ( s[st] != s[end] ) {
+                return false;
+            }
+            st++;
+            end--;
+
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int st = 0;
+        int end = s.size()-1;
+
+        while ( st < end ) {
+            if ( s[st] != s[end] ) {
+                return ( check(s, st+1, end) || check(s, st, end-1 ));
+            }
+            st++;
+            end--;
+        } 
+        return true;
+    }
+};
